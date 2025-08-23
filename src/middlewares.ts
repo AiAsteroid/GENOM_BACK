5c99eedfd4d29455b071bbe4829915b1bae75496
Сергей Бережной <ya_sergey@yahoo.com>
2025-08-23 14:23:59 +0300

    ф

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -4,17 +4,23 @@ import type ErrorResponse from "./interfaces/error-response.js";
 
 import { env } from "./env.js";
 
-export function notFound(req: Request, res: Response, next: NextFunction) {
-  res.status(404);
-  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
-  next(error);
-}
-
-export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, _next: NextFunction) {
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response<ErrorResponse>,
+  _next: NextFunction,
+) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
-  res.json({
+
+  const response: ErrorResponse = {
     message: err.message,
-    stack: env.NODE_ENV === "production" ? "🥞" : err.stack,
-  });
+    ...(env.NODE_ENV === "production"
+      ? { stack: "🥞" }
+      : err.stack
+        ? { stack: err.stack }
+        : {}),
+  };
+
+  res.json(response);
 }
