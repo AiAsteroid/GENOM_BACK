d219bddf09aa92aeca2deb5c4d82f49feec87a9f
Сергей Бережной <ya_sergey@yahoo.com>
2025-08-23 17:01:52 +0300

    ф

--- swagger.ts
+++ swagger.ts
@@ -11,7 +11,7 @@ const swaggerDefinition: SwaggerDefinition = {
   },
   servers: [
     {
-      url: `http://localhost:${process.env.PORT || 3000}`,
+      url: `https://gen-om.ru/api/`,
     },
   ],
 };
